e6cc5652de071d6e37ce54d667e227cd4a4c83c6
Miguel Ángel García Gómez <mgarcia@adaptavist.com>
2022-09-06 07:54:40 +0200

    Removing unnecessary lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,11 +23,3 @@ import { NgAisModule } from 'angular-instantsearch';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-We have an Angular application that uses the SSR mechanism. We have added Algolia as in the documentation: https://www.algolia.com/doc/guides/building-search-ui/going-further/server-side-rendering/angular/ The application renders some facets using the widget "ais-refinement-list".
-
-When the application in opened from a slow connection, and it's run with the SSR mechanism, the first time you click on a facet, while the page is still loading, the URL does not change (is not adding the search term params) and when is fully loaded, the element is unselected, going to the original state.
-
-But if the application in run without the SSR mechanism (only with "ng serve"), and with the slow connection, when a facet is clicked, the URL is changed, and when the page is fully loaded, the element keeps selected.
- */
